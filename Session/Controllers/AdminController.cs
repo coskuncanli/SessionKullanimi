ad150806638c05f85bbaa99686bfedbe43d2be95
coskuncanli <coskun.canli@gmail.com>
2023-03-17 16:07:43 +0300

    Proje dosyası ekle.

---
+++ Session/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Session.Models;
+
+namespace Session.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            string? ses = HttpContext.Session.GetString("username");
+            if(ses == null)
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+            else
+            {
+                ViewBag.User = ses;
+            }
+            return View();
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                User? x = UserInit.Init().FirstOrDefault(x=>x.Username==user.Username&&x.Password==user.Password);
+                if (x == null)
+                {
+                    ViewBag.LoginError = "Hatalı kullanıcı adı ya da şifre";
+                }
+                else
+                {
+                    HttpContext.Session.SetString("username", user.Username);
+                    return RedirectToAction("Index", "Admin");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Session/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace Session.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+
+    public class UserInit
+    {
+        public static List<User> Init()
+        {
+            return new List<User>()
+            {
+                new User(){Id = 0, Username = "Ali", Password = "123456"},
+                new User(){Id = 1, Username = "Can", Password = "123456"}
+            };
+        }
+    }
+}
